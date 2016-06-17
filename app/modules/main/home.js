950c3ce2c0b0220d6536f22a17c40d79306a5c4f
Jhonattan Manrique Alzate <Jhonattan Manrique Alzate>
2016-06-17 10:59:44 -0500

    change paths and math birthday formula

--- app/modules/main/home.js
+++ app/modules/main/home.js
@@ -1,12 +1,47 @@
 (function (angular) {
 
     angular.module('app').controller('Home', [
+        '$scope',
         Home])
 
-    function Home() {
+    function Home($scope) {
         var vm = this
 
-        vm.name = 'Jhonattan'
+        vm.mathBirthday = '?'
+
+        vm.birthDateOptions = {
+            format: 'dd MMMM yyyy',
+            change: calculateMathBirthday,
+            max: new Date()
+        }
+
+        function calculateMathBirthday() {
+            var birthDate = moment(this.value())
+            var currentDate = moment()
+
+            var nDay = 1
+            var mathBirthday = null
+
+            do {
+                var tempBirthDate = moment(this.value())
+                var mathBirthdayDays = Math.pow(10, nDay)
+                mathBirthday = tempBirthDate.add(mathBirthdayDays, 'days')
+                nDay = nDay + 1
+            }
+            while (mathBirthday.isBefore(currentDate))
+
+            vm.mathBirthdayDays = 'Your ' + mathBirthdayDays + '-day-old on:'
+
+            if (currentDate.diff(birthDate, 'years', true) >= 2) {
+                vm.mathBirthday = mathBirthday.format('DD MMMM YYYY')
+            }
+            else {
+                vm.mathBirthday = mathBirthday.diff(currentDate, 'weeks') + ' week(s)'
+            }
+
+
+            $scope.$apply()
+        }
     }
 
 })(angular)
